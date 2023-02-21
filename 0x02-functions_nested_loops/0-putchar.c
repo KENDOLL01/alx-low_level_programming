7f8a8b0c0f2995d62d59cf2ed845667a6935b421
KENDOLL01 <keneilwesekonya01@gmail.com>
2023-02-21 09:32:46 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
  * main - prints _putchar, followed by a new line.
  *
- * Return: Always 0.
+ * Return: 0.
 */
 int main(void)
 
 {
-	_putchar('95');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
