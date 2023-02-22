f00188c7323c18d15c7bb64f0e851746a50faabb
KENDOLL01 <keneilwesekonya01@gmail.com>
2023-02-22 09:05:08 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'n'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
