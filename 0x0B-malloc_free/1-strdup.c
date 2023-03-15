570e5b3543a7c5c5a836cb96c632c257cd9bc2d2
KENDOLL01 <keneilwesekonya01@gmail.com>
2023-03-15 09:43:09 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,6 +10,7 @@
 
 char *_strdup(char *str)
 {
+
 	char *aaa;
 	int i, r = 0;
 	if (str == NULL)
@@ -17,13 +18,22 @@ char *_strdup(char *str)
 		return (NULL);
 
 	i = 0;
+
 	while (str[i] != '\0')
+
 		i++;
+
+
 	aaa = malloc(sizeof(char) * (i + 1));
+
+
 	if (aaa == NULL)
 		return (NULL);
+
+
 	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
 
-               return (aaa);
+
+		return (aaa);
 }
